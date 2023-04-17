b5c190cad4b1ef45f325ebc13e9f0c2275a02d86
doran08 <95293707+doran08@users.noreply.github.com>
2023-04-17 17:45:16 +0200

    Create Game_Loader.lua

---
+++ Game_Loader.lua
@@ -0,0 +1,13 @@
+
+local url = "https://raw.githubusercontent.com/Doran342545345/Scriptshowcaeses/main/games54"
+
+local games = {
+
+}
+
+for i,v in next, games do
+    games[i] = table.concat(v:split(' '), '_')
+end
+
+local name = games[game.PlaceId] or games[game.GameId]
+return loadstring(game:HttpGet(url.. "/"..(name or "Universal")..".lua"))()
